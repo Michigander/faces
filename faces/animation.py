24929eca9f129f7f1779fd9a3a77b345eb18ec82
Gordon Finnie <gordon.m.finnie.iii@gmail.com>
2017-04-19 22:27:47 -0400

    animation is extensible. face is extensible.

--- faces/animation.py
+++ faces/animation.py
@@ -1,16 +1,25 @@
-
 class Animation(object):
-    
+    """
+    The extensible base class for a face animation.
+
+    """
     def __init__(self, face):
-        """ Construct. """
-        
+        """ Construct Animation """
+
         self._face = face
 
         self._mask = mask
 
     def play():
         """ Play an animation. """
-        
-        self._face 
 
-=>
+        self._face
+
+class Wink(Animation):
+    """ A simple animation.
+
+    """
+    def __init__(self, face):
+        """ Construct Wink Animation """
+        
+        super(Wink,self)__init__()
